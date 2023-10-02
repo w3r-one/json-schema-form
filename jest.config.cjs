406c630cffaa0fd548392b86ab3d87b7af01d225
Cyrille Perois <cyrille.perois@w3r.one>
2023-10-02 14:03:33 +0200

    chore: ignore lib directory in tests

--- jest.config.cjs
+++ jest.config.cjs
@@ -162,9 +162,9 @@ module.exports = {
 	// ],
 
 	// An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
-	// testPathIgnorePatterns: [
-	//   "/node_modules/"
-	// ],
+	testPathIgnorePatterns: [
+	  "lib"
+	],
 
 	// The regexp pattern or array of patterns that Jest uses to detect test files
 	// testRegex: [],
